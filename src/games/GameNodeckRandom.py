8426782dfd8a843373c1aad986d6e18151b440db
m-gorg <91372239+m-gorg@users.noreply.github.com>
2024-02-23 20:07:15 +0200

    Created game no deck

---
+++ src/games/GameNodeckRandom.py
@@ -0,0 +1,12 @@
+from Game import Game
+from Deck import Deck
+from Card import Card
+from random import randint, choice
+
+# Game variant with all cards allready drawn
+# 4+ players advised
+
+class GameNodeckRandom(Game):
+    
+    pass
+
